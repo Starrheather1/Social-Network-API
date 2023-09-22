ef22886b4319b37342f8968c18fed7da6ea8071d
Heather Starr <starr.heather1@yahoo.com>
2023-09-22 11:12:28 -0700

    setup thoughtController

---
+++ controllers/thoughtController.js
@@ -0,0 +1,22 @@
+const { User } = require('../models');
+
+module.exports = {
+  async getUsers(req, res) {
+
+  },
+  async getSingleUser(req, res) {
+
+  },
+  async createUser(req, res) {
+
+  },
+  async deleteUser(req, res) {
+
+  },
+  async addFriend(req, res) {
+
+  },
+  async deleteFriend(req, res) {
+
+  }
+};
